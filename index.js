5ae8659e9ba9d8abbcc09039d83481d6ba489604
Josh Bain <joshuambain@gmail.com>
2024-07-24 12:05:58 -0400

    added fetch in index.js that accesses db.json

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 alert("Site still under construction :/")
 
 console.log("Really tho it's under construction...")
-
+fetch("http://localhost:3000/schools")
+.then(response => response.json())
+.then(schools => {
+    console.log(schools)
+})
 
 //note Can create a "sample" json file by standing up a non-existant json server (refer to code challenge for specfic commands)
