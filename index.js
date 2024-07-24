2fc9ddf055c4c8eb5230645a98e01dece505c73b
Josh Bain <joshuambain@gmail.com>
2024-07-24 13:35:11 -0400

    wrote initial forEach function that is incomplete and not reflecting in the DOM

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 alert("Site still under construction :/")
 
+const schoolsList = document.getElementById('schools-list')
+console.log(schoolsList)
+
 console.log("Really tho it's under construction...")
 fetch("http://localhost:3000/schools")
 .then(response => response.json())
 .then(schools => {
-    console.log(schools)
+    schools.forEach(addSchoolToList)
 })
 
+function addSchoolToList(school) {
+    const liElement = document.createElement('li')
+    const nameElement = document.createElement('h3')
+    nameElement.textContent = `School Name: ${school.name}`
+
+    liElement.appendChild(nameElement)
+    schoolsList.appendChild(liElement)
+
+}
+
+
+// above code was attempt to follow along with ricardo but for my own school example (stopped following him, but current amount seems reasonably illustrative)
 //note Can create a "sample" json file by standing up a non-existant json server (refer to code challenge for specfic commands)
