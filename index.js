2394ba356b89d36a5033cf920dc777f29bee4c3a
Josh Bain <joshuambain@gmail.com>
2024-07-24 12:03:14 -0400

    created db.json file with sample data

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 alert("Site still under construction :/")
 
 console.log("Really tho it's under construction...")
+
+
+//note Can create a "sample" json file by standing up a non-existant json server (refer to code challenge for specfic commands)
