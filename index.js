2639d3c974c2d4847a7d402b330868e9b0996154
Josh Bain <joshuambain@gmail.com>
2024-07-28 12:10:34 -0400

    built event change event listener and displaySchoolDataFunctions that selectively add elements based on filterByStateCodeElement selection

--- index.js
+++ index.js
@@ -1,25 +1,104 @@
 alert("Site still under construction :/")
 
-const schoolsList = document.getElementById('schools-list')
-console.log(schoolsList)
+const schoolsListElement = document.getElementById('schools-list')
+console.log(schoolsListElement)
 
 console.log("Really tho it's under construction...")
 fetch("http://localhost:3000/schools")
 .then(response => response.json())
+// .then(console.log(response))
 .then(schools => {
-    schools.forEach(addSchoolToList)
+    filterByStateCodeElement.addEventListener('change', () => {
+        console.log("Logging if the change event listener is working")
+        displaySchoolData(schools)
+    })
+    // schools.forEach(addSchoolToList)
+    
 })
 
 function addSchoolToList(school) {
     const liElement = document.createElement('li')
-    const nameElement = document.createElement('h3')
-    nameElement.textContent = `School Name: ${school.name}`
+    // const nameElement = document.createElement('h3')
+    liElement.textContent = `School Name: ${school.name}`
 
-    liElement.appendChild(nameElement)
-    schoolsList.appendChild(liElement)
+    // liElement.appendChild(nameElement)
+    schoolsListElement.appendChild(liElement)
 
 }
 
 
-// above code was attempt to follow along with ricardo but for my own school example (stopped following him, but current amount seems reasonably illustrative)
+// above code was attempt to follow along with ricardo's example but applying my own school focus (stopped following him, but current amount seems reasonably illustrative)
+// burger-project-2 on Ricardo's github has the full project
 //note Can create a "sample" json file by standing up a non-existant json server (refer to code challenge for specfic commands)
+
+// const cryptocurrencyListElement = document.getElementById('cryptocurrency-list')
+const filterByStateCodeElement = document.getElementById("filter-by-state-code")
+
+
+// fetch("http://localhost:3000/schools")
+// .then(response => {
+//     if(response.ok){
+//         response.json().then(schools => {
+//             displaySchoolData(schools)
+//             filterByStateCodeElement.addEventListener('change', () => {
+//                 displaySchoolData(schools)
+//             })
+//         })
+//     }
+//     else{
+//         alert("Error: Unable to return API data.")
+//     }
+// })
+
+// function addCryptocurrencyToList(cryptocurrency) {
+//     const liElement = document.createElement('li')
+//     liElement.textContent = `${cryptocurrency.name} (${cryptocurrency.symbol}): Rank # ${cryptocurrency.rank}`
+//     cryptocurrencyListElement.appendChild(liElement)
+// }
+
+function displaySchoolData(schools) {
+    // schoolsListElement.innerHTML = ""
+    if(filterByStateCodeElement.value === "AK") {
+        schools.forEach(school => {
+            if(school.statecode === "AK") {
+                addSchoolToList(school)
+            }
+        })
+    }
+    else if (filterByStateCodeElement.value === "AL") {
+        schools.forEach(school => {
+            if(school.statecode === "AL") {
+            addSchoolToList(school)
+        }
+        })
+        
+        
+    }
+    else if (filterByStateCodeElement.value === "AR") {
+        schools.forEach(school => {
+            if(school.statecode === "AR") {
+            addSchoolToList(school)
+        }
+        })
+    }
+    else if (filterByStateCodeElement.value === "AZ") {
+        schools.forEach(school => {
+            if(school.statecode === "AZ") {
+            addSchoolToList(school)
+        }
+        })
+    }
+    // console.log(cryptocurrencyFilterElement.value)
+    // console.log(cryptocurrencies)
+}
+
+
+// else if (filterByStateCodeElement.value==="AK") {
+//     schools.forEach(cryptocurrency => {
+//         if(Number(cryptocurrency.rank) <=50) {
+//             addSchoolToList(cryptocurrency)
+//         }
+//     })
+// }
+
+// Above code leveraged from 7.26 class time - filter on boolean is very important
